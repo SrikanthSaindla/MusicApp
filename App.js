1d452ec6c0a6dc2fbaef9614fe7ca18d8ae2c013
Srikanth Saindla <110750154+SrikanthSaindla@users.noreply.github.com>
2023-01-30 21:03:49 +0530

    welcome note

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableWithoutFeedback, View,Image,TextInput,Button } from 'react-native';
 
 export default function App() {
+  const [name,setNAme]=useState('')
   return (
     <View style={styles.container}>
-      <Text> Hello srikanth</Text>
+      <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      <Text> Hello {name}</Text>
       <Text>Welcome</Text>
+      <TextInput
+        style={{
+          height: 40,
+          width:100,
+          borderColor: 'gray',
+          borderWidth: 2,
+          padding:6,
+        }}
+        placeholder="Enter Name"
+        onChangeText={new1 => setNAme(new1)}
+        defaultValue={name}
+      />
+      <Button
+        onPress={() => {
+          setNAme(name);
+        }}
+         
+title='Login'
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'skyblue',
+    backgroundColor: 'lightpink',
     alignItems: 'center',
     justifyContent: 'center',
     Color:'white',
